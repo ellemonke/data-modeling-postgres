82d290f8e7d9be22eb2c3b7d802e3f83f999e3fd
Ellen Hsu <ixnayontheamspay@gmail.com>
2020-05-16 10:48:43 -0700

    py files complete

--- etl.py
+++ etl.py
@@ -9,51 +9,81 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = pd.read_json(filepath, lines=True)
+    songs_df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data =
+    songs = songs_df[['song_id', 'title', 'artist_id', 'year', 'duration']]
+    song_data = songs.values[0].tolist()
     cur.execute(song_table_insert, song_data)
 
     # insert artist record
-    artist_data =
+    artists = songs_df[['artist_id', 'artist_name',
+                        'artist_location', 'artist_latitude', 'artist_longitude']]
+    artist_data = artists.values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = pd.read_json(filepath, lines=True)
+    log_df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df =
+    log_df = log_df.loc[log_df.page == "NextSong"]
 
     # convert timestamp column to datetime
-    t =
+    t = log_df.ts.values
+    t = t.reshape(-1, 1)
 
-    # insert time data records
-    time_data =
-    column_labels =
-    time_df =
+    time_data = []
+
+    for i in range(len(t)):
+        try:
+            timestamp = ()
+            start_time = dt.datetime.fromtimestamp(t[i][0] / 1000)
+            start_time2 = dt.datetime.strptime(
+                str(start_time), '%Y-%m-%d %H:%M:%S.%f')
+            timestamp = (start_time, start_time2.strftime('%H'), start_time2.strftime('%d'),
+                         start_time2.strftime(
+                             '%U'), start_time2.strftime('%m'),
+                         start_time2.strftime('%Y'), start_time2.strftime('%u'))
+            time_data.append(timestamp)
+        except KeyError:
+            pass
+
+    time_data = tuple(time_data)
+
+    column_labels = ('start_time', 'hour', 'day',
+                     'week', 'month', 'year', 'weekday')
+
+    time_df = pd.DataFrame(columns=column_labels, data=time_data)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df =
+    user_df = log_df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = user_df.dropna()
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
     # insert songplay records
-    for index, row in df.iterrows():
+    for index, row in log_df.iterrows():
 
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, [row.song, row.artist, row.length])
+        results = cur.fetchone()
+
+        if results:
+            songId, artistId = results
+        else:
+            songId, artistId = None, None
 
         # insert songplay record
-        songplay_data =
+        timestamp = dt.datetime.fromtimestamp(row.ts / 1000)
+        songplay_data = (timestamp, row.userId, row.level, songId, artistId,
+                         row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -80,6 +110,7 @@ def main():
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=sparkifydb user=postgres password=3ll3nhsu")
     cur = conn.cursor()
+    conn.set_session(autocommit=True)
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
