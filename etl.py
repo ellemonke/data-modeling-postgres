375bf4e5dc8fa00d8fc1b0e94cae95cc919b9161
Ellen Hsu <ixnayontheamspay@gmail.com>
2020-05-16 15:02:04 -0700

    edit for different version of python

--- etl.py
+++ etl.py
@@ -109,7 +109,7 @@ def process_data(cur, conn, filepath, func):
 
 def main():
     conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=sparkifydb user=postgres password=3ll3nhsu")
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
     conn.set_session(autocommit=True)
 
